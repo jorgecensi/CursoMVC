9e02e11622b12e279fba5ba9b714583f94a49e22
Jorge <jorgecensi@gmail.com>
2016-09-15 18:46:35 -0300

    Commit Inical

---
+++ src/JLC.CursoMVC.UI.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JLC.CursoMVC.UI.Mvc.Startup))]
+namespace JLC.CursoMVC.UI.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
