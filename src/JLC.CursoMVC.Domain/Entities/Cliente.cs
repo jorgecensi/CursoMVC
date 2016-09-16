fb9ea030956010058c63ca91af59af4ffc7b0260
Jorge <jorgecensi@gmail.com>
2016-09-15 22:06:15 -0300

    adicionado as Classes de Domínio

---
+++ src/JLC.CursoMVC.Domain/Entities/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JLC.CursoMVC.Domain.Entities
+{
+    public class Cliente
+    {
+
+        public Cliente()
+        {
+            ClienteId = Guid.NewGuid();
+        }
+
+
+        public Guid ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string CPF { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //propriedade virtual: significa que pode ser sobreescrita
+        // também utilizada para utilizar o lazyloading
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+
+
+    }
+}
---
+++ src/JLC.CursoMVC.Domain/Entities/Endereco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JLC.CursoMVC.Domain.Entities
+{
+    public class Endereco
+    {
+        public Endereco()
+        {
+            EnderecoId = Guid.NewGuid();
+        }
+
+        public Guid EnderecoId { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+        
+        //fk da tabela Cliente
+        public Guid ClienteId { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+
+    }
+}
