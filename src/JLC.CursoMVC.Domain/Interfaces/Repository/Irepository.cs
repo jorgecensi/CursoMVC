705a41c055caefc03d31552a112b42e039e0d9b4
Jorge <jorgecensi@gmail.com>
2016-10-17 20:05:02 -0200

    Criando o repositorio genérico

---
+++ src/JLC.CursoMVC.Domain/Interfaces/Repository/Irepository.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace JLC.CursoMVC.Domain.Interfaces.Repository
+{
+    public interface Irepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity Adicionar(TEntity obj);        
+        TEntity OberPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        TEntity Atualizar(TEntity obj);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int SaveChanges();
+
+
+
+    }
+}
---
+++ src/JLC.CursoMVC.Infra.Data/Repository/Repository.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using JLC.CursoMVC.Domain.Interfaces.Repository;
+using JLC.CursoMVC.Infra.Data.Context;
+using System.Data.Entity;
+using System.Linq;
+
+namespace JLC.CursoMVC.Infra.Data.Repository
+{
+    public class Repository<Tentity> : Irepository<Tentity> where Tentity : class
+    {
+        protected CursoMvcContext Db;
+        protected DbSet<Tentity> DbSet;
+
+        public Repository()
+        {
+            //criando um atalho, para não digitar Set<Tentity>() em todo método
+            Db = new CursoMvcContext();
+            DbSet = Db.Set<Tentity>();
+        }
+
+        public virtual Tentity Adicionar(Tentity obj)
+        {
+            //adiciona na memória do EF
+            var objReturn = DbSet.Add(obj);            
+            SaveChanges();
+            return objReturn;
+        }
+
+        public virtual Tentity Atualizar(Tentity obj)
+        {
+            var entry = Db.Entry(obj);
+            DbSet.Attach(obj);
+            entry.State = EntityState.Modified;
+            SaveChanges();
+
+            return obj;
+        }
+
+        public IEnumerable<Tentity> Buscar(Expression<Func<Tentity, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual Tentity OberPorId(Guid id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public IEnumerable<Tentity> ObterTodos()//(int inicio, int qtd) 
+        {
+            return DbSet.ToList(); //Take(qtd).Skip(inicio).ToList(); 
+        }
+
+        public virtual void Remover(Guid id)
+        {
+            DbSet.Remove(OberPorId(id));
+            SaveChanges();
+        }
+
+        public int SaveChanges()
+        {
+            return Db.SaveChanges();
+        }
+    }
+}
