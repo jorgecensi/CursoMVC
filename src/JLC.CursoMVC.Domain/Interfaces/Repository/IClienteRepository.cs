364fdb29bb75cd6e5fbbbabd27d0b70cbe645af2
Jorge <jorgecensi@gmail.com>
2016-10-17 20:22:03 -0200

    criado repositorio de cliente

---
+++ src/JLC.CursoMVC.Domain/Interfaces/Repository/IClienteRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using JLC.CursoMVC.Domain.Entities;
+
+namespace JLC.CursoMVC.Domain.Interfaces.Repository
+{
+    public interface IClienteRepository : Irepository<Cliente>
+    {
+        Cliente ObterPorCpf(string cpf);
+        Cliente ObterPorEmail(string email);
+    }
+}
---
+++ src/JLC.CursoMVC.Infra.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JLC.CursoMVC.Domain.Entities;
+using JLC.CursoMVC.Domain.Interfaces.Repository;
+using System.Linq;
+
+namespace JLC.CursoMVC.Infra.Data.Repository
+{
+    public class ClienteRepository : Repository<Cliente>, IClienteRepository
+    {
+        public Cliente ObterPorCpf(string cpf)
+        {
+            //return Db.Clientes.FirstOrDefault(c => c.CPF == cpf);
+            //return Buscar(c => c.CPF == cpf).SingleOrDefault(); //somente o registro unico
+            return Buscar(c => c.CPF == cpf).FirstOrDefault(); //pego o primero da fila
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            return Buscar(e => e.Email == email).FirstOrDefault();
+        }
+
+        ////excluir sem remover da base
+        //public override void Remover(Guid id)
+        //{
+        //    //precisa ter um campo chamado Excluido na tabela
+        //    Db.Clientes.Find(id).Excluido == true;
+        //    SaveChanges();
+        //}
+    }
+}
