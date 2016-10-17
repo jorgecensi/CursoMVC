0f5bea2351768fb8db04062be2436da42a62eb1b
Jorge <jorgecensi@gmail.com>
2016-10-17 21:36:51 -0200

    Adicionando o ClienteViewModel

---
+++ src/JLC.CursoMvc.Application/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace JLC.CursoMvc.Application.ViewModels
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public Guid ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(150,ErrorMessage ="Máximo {0} caracteres")]
+        [MinLength(2,ErrorMessage ="Mínimo {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo E-mail")]
+        [MaxLength(100, ErrorMessage = "Máximo {0} caracteres")]
+        [EmailAddress(ErrorMessage = "Preencha um E-mail válido")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo CPF")]
+        [MaxLength(11, ErrorMessage = "Máximo {0} caracteres")]
+        [DisplayName("CPF")]
+        public string CPF { get; set; }
+
+        [Display(Name ="Data de Nascimento")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage ="Data em formato inválido")]
+        public DateTime DataNascimento { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Ativo { get; set; }
+
+
+
+    }
+}
