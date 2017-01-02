2209b4707289831b65a813f75beee6465d4187e4
Jorge <jorgecensi@gmail.com>
2017-01-02 13:11:17 -0200

    Classe de testes da Specification do CPF

---
+++ tests/JLC.CursoMvc.Domain.Tests/Specification/CPFSpecification.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JLC.CursoMVC.Domain.Entities;
+using JLC.CursoMVC.Domain.Specifications.Clientes;
+
+namespace JLC.CursoMvc.Domain.Tests.Specification
+{
+    [TestClass]
+    public class CPFSpecification
+    {
+        public Cliente Cliente { get; set; }
+
+        [TestMethod]
+        public void CPF_Valido_True()
+        {
+            Cliente = new Cliente()
+            {
+                CPF = "05665579912"
+            };
+
+            var cpf = new ClienteDeveTerCpfValidoSpecification();
+            Assert.IsTrue(cpf.IsSatisfiedBy(Cliente));
+
+        }
+
+        [TestMethod]
+        public void CPF_Valido_False()
+        {
+            Cliente = new Cliente()
+            {
+                CPF = "05665579911"
+            };
+
+            var cpf = new ClienteDeveTerCpfValidoSpecification();
+            Assert.IsFalse(cpf.IsSatisfiedBy(Cliente));
+
+        }
+    }
+}
