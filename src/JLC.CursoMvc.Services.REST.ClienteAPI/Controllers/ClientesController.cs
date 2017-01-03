f53a55575695bbca61e663552b1003e8c6667911
Jorge <jorgecensi@gmail.com>
2017-01-02 22:56:43 -0200

    Criado Projeto de Serviços Adicionado API REST de Clientes.

---
+++ src/JLC.CursoMvc.Services.REST.ClienteAPI/Controllers/ClientesController.cs
@@ -0,0 +1,49 @@
+﻿using JLC.CursoMvc.Application.Interfaces;
+using JLC.CursoMvc.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JLC.CursoMvc.Services.REST.ClienteAPI.Controllers
+{
+    public class ClientesController : ApiController
+    {
+
+        private readonly IClienteAppService _clienteAppService;
+
+        public ClientesController(IClienteAppService clienteAppService)
+        {
+            _clienteAppService = clienteAppService;
+        }
+
+        // GET: api/Clientes
+        public IEnumerable<ClienteViewModel> Get()
+        {
+            return _clienteAppService.ObterTodos();
+        }
+
+        // GET: api/Clientes/5
+        public ClienteViewModel Get(Guid id)
+        {
+            return _clienteAppService.ObterPorId(id);
+        }
+
+        // POST: api/Clientes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Clientes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Clientes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/JLC.CursoMvc.Services.REST.ClienteAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JLC.CursoMvc.Services.REST.ClienteAPI.WebApiApplication" Language="C#" %>
